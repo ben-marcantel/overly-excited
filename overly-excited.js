4f0806f3252dd6c850f73bbbd7be2422210e98cd
MyMac <mymac@MyMacs-MacBook-Pro.local>
2017-12-01 13:53:52 -0600

    finally

--- overly-excited.js
+++ overly-excited.js
@@ -2,28 +2,66 @@
 
 
 
-const sentence= ["The ", "walrus ", "danced ", "through ", "the ", "trees ", "in ", "the ", "light ", "of ", "the ", "moon"];
+const sentence = ["The ", "walrus ", "danced ", "through ", "the ", "trees ", "in ", "the ", "light ", "of ", "the ", "moon"];
 
 
-
-// The addExcitement function should accept the array as the sole argument
-function addExcitement (theWordArray) {
-    for (i=0; i<sentence.length; i++) {
-        
-        if (/*how to get counter number here?*/i % 3 = 0) {
-            console.log (theWordArray + "!")
-
-        }else console.log(theWordArray)
-
+for (i=0;i<sentence.length;i++) {
+   let phrase= "";
+    for(x=0; x<=i; x++) {
+        if (sentence.length)
+        phrase+=sentence[x];
     }
-    /*
-      Write a `for` loop that iterates over the array argument and
-      outputs the words.
-     */
+    if (x%3===0) {
+     console.log(phrase + "!")   
+    } else console.log(phrase);
 }
+    
 
-// Invoke the function and pass in the array
-addExcitement(sentence)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
